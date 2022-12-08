794a57ff4be41b013712766f1e25c49677000ff0
Justine Tihno <justintihno@gmail.com>
2022-12-08 09:35:55 -0800

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
  * main- for printing size of characters
  *
  * Return- returns a 0
- */
+ i*/
 int main(void)
 {
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
