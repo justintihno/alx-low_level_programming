95d709dbf486b10b9213d37bc46140b3dd72a9e7
Justine Tihno <justintihno@gmail.com>
2022-12-09 11:57:08 -0800

    coding in c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 /**
  * main: Entry point
- * Description: prints the alphabet in lowercase\n
+ * Description: prints the alphabet in lowercase followed by a new line
  * Return: Always 0 (success)
  */
 int main(void)
