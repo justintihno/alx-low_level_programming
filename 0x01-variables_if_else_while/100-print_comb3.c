9b32f09fdc4ac332ddb5524c1df613b1fbe7243b
Justine Tihno <justintihno@gmail.com>
2022-12-13 03:55:32 -0800

    c loops

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,14 @@ int main(void)
 
 {
 	int i, j;
-	
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
+			putchar(j + '0');
 			putchar(',');
 			putchar(' ');
-			putchar(j + '0');
 			}
 	}
 	putchar('\n');
