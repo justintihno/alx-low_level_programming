48983850fafb370fa01c91f4294d9d394493c036
Justine Tihno <justintihno@gmail.com>
2022-12-14 10:37:21 -0800

    nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,13 @@
 void times_table(void)
 {
 	int i, j, tens, ones, result;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-	for (j = 0; j <= 9; j++)
+	for (j = 1; j <= 9; j++)
 	{
 		result = i * j;
 		tens = result / 10;
