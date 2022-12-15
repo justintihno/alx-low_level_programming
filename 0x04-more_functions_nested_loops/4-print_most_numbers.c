c5a61fd58e1dc8b55d0ae7f43c9384a38e50c515
Justine Tihno <justintihno@gmail.com>
2022-12-15 07:53:39 -0800

    more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,9 @@
 #include "main.h"
-/**
- * print_most_numbers - prints 0 - 9 except for 2, 4
- * Description: prints
- * Return: void
- */
+
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		if (i !=2 && i != 4)
