24fa0ecd75d0504c2762e44560cb90819fad1288
Justine Tihno <justintihno@gmail.com>
2022-12-20 04:11:14 -0800

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for
-		(str[i] != '\0';
-		 i += 2)
+	for (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
 		_putchar(str[i]);
+		}
+		i++;
+	}
 	_putchar('\n');
 }
