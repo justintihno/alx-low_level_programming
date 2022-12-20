52ff6c0a51649c9f2b596c69876e44cb99dc03ee
Justine Tihno <justintihno@gmail.com>
2022-12-20 04:02:45 -0800

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,12 @@ void puts_half(char *str)
 	int i, len;
 
 	for
-		(len = 0; str[len] != '\0'; len++);
+		(len = 0;
+		 str[len] != '\0';
+		 len++);
 	for
-		(i = (len - 1) / 2 + 1; i < len; i++)
+		(i = (len - 1) / 2 + 1;
+		 i < len; i++)
 			_putchar(str[i]);
 	_putchar('\n');
 }
