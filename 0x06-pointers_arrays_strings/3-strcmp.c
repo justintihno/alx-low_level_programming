c543c274120b7245e476fa7fac5049cd63b5891f
Justine Tihno <justintihno@gmail.com>
2022-12-22 06:24:35 -0800

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,8 +4,8 @@
  * _strcmp - Compare two strings
  * @s1: First string
  * @s2: Second string
- * Return: An integer less than, equal to, or greater than zero
- * if s1 is found, respectively, to be less than, to match, or be greater than s2
+ * Return: An integer less than, equal to, or greater than zero if s1
+ * is found, respectively, to be less than, to match, or be greater than s2
  */
 
 int _strcmp(char *s1, char *s2)
@@ -14,6 +14,6 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; s1[i] == s2[i]; i++)
 	if (s1[i] == '\0')
-	return 0;
+	return (0);
 	return (s1[i] - s2[i]);
 }
