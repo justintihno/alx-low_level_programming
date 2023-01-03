446b09dcefe8322417108c94252864297329f431
Justine Tihno <justintihno@gmail.com>
2023-01-03 07:02:41 -0800

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,10 +9,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	for (i = 0; a[i][7]; i++)
+	for (i = 0; a < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			putchar(a[i][j]);
-		putchar('\n');
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
 	}
 }
