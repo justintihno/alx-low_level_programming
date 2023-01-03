4b8b15f649bdf408c1e5f3cef83e6a50685ad706
Justine Tihno <justintihno@gmail.com>
2023-01-03 05:24:16 -0800

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+	unsigned char *d = dest;
+	const unsigned char *s = src;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		d[i] = s[i];
 	}
 	return (dest);
 }
