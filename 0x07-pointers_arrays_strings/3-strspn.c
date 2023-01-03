9efe441efdbcabbe4420308af6e16bc3221d1814
Justine Tihno <justintihno@gmail.com>
2023-01-03 06:13:45 -0800

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,21 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
+	unsigned int count = 0;
+	int i,j;
 
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
+		{
 			if (s[i] == accept[j])
+			{
+				count++;
 				break;
+			}
 	}
-	return (i);
+		if (!accept[j])
+			break;
+	}
+	return (count);
 }
