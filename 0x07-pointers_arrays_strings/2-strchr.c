ed32ee5f0a8b6bc0374e4c2a66d8e7ac3683f58b
Justine Tihno <justintihno@gmail.com>
2023-01-03 06:01:38 -0800

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,10 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; index++)
+	for (index = 0; s[index] >= '\0'; index++)
 	{
 		if (s[index] == c)
 			return (s + index);
 	}
-	return (NULL);
+	return ('\0');
 }
