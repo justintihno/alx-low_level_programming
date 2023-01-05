928b3edebbcf4612b550c6db709b85c2215ade06
Justine Tihno <justintihno@gmail.com>
2023-01-05 01:07:02 -0800

    raised recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,12 +8,7 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (- 1);
-	else if (n == 0 || n == 1)
-		return (n);
-	else
-		return (_find_sqrt(n, 1));
+	return (_find_sqrt(n, 1));
 }
 
 /**
