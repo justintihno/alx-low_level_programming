7b1fde8048d4ed6eefe781d00ccb2964aa2cd585
Justine Tihno <justintihno@gmail.com>
2023-01-05 21:59:14 -0800

    palindrome recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,19 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - Finds the length of a string
+ * @s: The string to be measured
+ * Return: The length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
  * is_palindrome - check a string if is a palindrome
  * @s: the string to be checked
  * Return: returns 1 if a string is a palindrome and 0 if not.
@@ -8,9 +21,12 @@
 
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
+	int length;
+
+	length = _strlen_recursion(s);
+	if (length <= 1)
 		return (1);
-	if (*s != *(s + _strlen_recursion(s) - 1))
+	if (*s != s[length - 1])
 		return (0);
 	return (is_palindrome(s + 1));
 }
