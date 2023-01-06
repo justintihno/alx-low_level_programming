e5e5851819e5ba4fe3a59ff16e80a70d62e6a7ba
Justine Tihno <justintihno@gmail.com>
2023-01-05 22:07:29 -0800

    palindrome recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return (0);
+		return (1);
 	return (1 + _strlen_recursion(s + 1));
 }
 
@@ -28,5 +28,5 @@ int is_palindrome(char *s)
 		return (1);
 	if (*s != s[length - 1])
 		return (0);
-	return (is_palindrome(s + 1));
+	return (is_palindrome(s + 1, s + length - 2));
 }
