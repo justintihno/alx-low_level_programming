1a400cf90ff9893f2fc289c9a93a2badcfb0c5fd
Justine Tihno <justintihno@gmail.com>
2023-01-13 22:35:01 -0800

    task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc and checks for errors
+ * @b: Number of bytes to allocate
+ * Return: Pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
