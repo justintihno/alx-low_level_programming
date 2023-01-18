e06f9f05f216a638945dafb56630b92a5696fc1f
Justine Tihno <justintihno@gmail.com>
2023-01-18 10:37:51 -0800

    task

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,11 @@
 #ifndef ABS_H
 #define ABS_H
+
+/**
+ * ABS - computes the absolute value of a number x
+ * @x: the number to be evaluated
+ * Return: the absolute value of x
+ */
+
 #define ABS(x) ((x < 0) ? (-x) : (x))
 #endif
