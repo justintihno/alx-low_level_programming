e70745de3b5aa1dcb2703d7dae84c9d208d423db
Justine Tihno <justintihno@gmail.com>
2023-01-19 22:34:35 -0800

    task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
