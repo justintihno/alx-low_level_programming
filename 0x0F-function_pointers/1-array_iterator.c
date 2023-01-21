676687a0264cac3beafb737a96864282bc073711
Justine Tihno <justintihno@gmail.com>
2023-01-21 09:49:35 -0800

    task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 
 	if (!array || !action)
-		return (void);
+		return ();
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
