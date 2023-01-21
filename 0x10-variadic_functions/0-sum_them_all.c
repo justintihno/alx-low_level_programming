169182eb0ac580b65e7a5f9f62be83ab4b953aa9
Justine Tihno <justintihno@gmail.com>
2023-01-20 23:45:54 -0800

    task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: number of parameters
- * Return: 0 if n == 0, else sum of all parameters
+ * Return: sum
  */
 
 int sum_them_all(const unsigned int n, ...)
