f4ed682d912f7a2b8ce041562e0ea61d082a6b86
Justine Tihno <justintihno@gmail.com>
2023-02-01 00:07:49 -0800

    task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,10 @@
-section .data
-hello db 'Hello, Holberton', 0
-newline db 10, 0
-
 section .text
 global _start
 
 _start:
 ; print the string
-mov rdx 13 ; message length
-mov rcx, hello ; message to write
+mov rdx 14 ; message length
+mov rcx, msg ; message to write
 mov rbx, 1 ; file descriptor (stdout)
 mov rax, 1 ; system call number (sys_write)
 syscall
@@ -24,3 +20,7 @@ syscall
 mov rax, 60 ; system call number (sys_exit)
 xor rdi, rdi ; exit code (0)
 syscall
+
+section .data
+msg db 'Hello, Holberton', 0
+newline db 10, 0
