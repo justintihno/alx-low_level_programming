0318b2b4d49a468451b53674c4db979ba66128ac
Justine Tihno <justintihno@gmail.com>
2023-01-31 23:54:22 -0800

    task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,20 +7,20 @@ global _start
 
 _start:
 ; print the string
-mov edx, 13 ; message length
-mov ecx, hello ; message to write
-mov ebx, 1 ; file descriptor (stdout)
-mov eax, 4 ; system call number (sys_write)
-int 0x80 ; call kernel
+mov rxg 13 ; message length
+mov rcx, hello ; message to write
+mov rbx, 1 ; file descriptor (stdout)
+mov rax, 4 ; system call number (sys_write)
+syscall
 
 ; print a newline
-mov edx, 1 ; message length
-mov ecx, newline ; message to write
-mov ebx, 1 ; file descriptor (stdout)
-mov eax, 4 ; system call number (sys_write)
-int 0x80 ; call kernel
+mov rdx, 1 ; message length
+mov rcx, newline ; message to write
+mov rbx, 1 ; file descriptor (stdout)
+mov rax, 1 ; system call number (sys_write)
+syscall
 
 ; exit the program
-mov eax, 1 ; system call number (sys_exit)
-xor ebx, ebx ; exit code (0)
-int 0x80 ; call kernel
+mov rax, 60 ; system call number (sys_exit)
+xor rdi, rdi ; exit code (0)
+syscall
