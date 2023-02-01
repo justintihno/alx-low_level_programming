fad0b2d7c37251fdb3636964a80dc673dea5777e
Justine Tihno <justintihno@gmail.com>
2023-01-31 23:14:07 -0800

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,6 +3,7 @@
 
 /**
  * print_list - function that prints all the elements of a linked list_t list
+ * struct list_s - singly linked list
  * @h: pointer to head node of linked list
  * Return: number of nodes in the linked list
  */
