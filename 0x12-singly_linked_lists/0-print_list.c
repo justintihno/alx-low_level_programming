c871cafe57c71e8cc415cc8c6ad11011f9172a90
Justine Tihno <justintihno@gmail.com>
2023-01-31 22:02:09 -0800

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
+
+/**
+ * print_list - prints all elements of a list_t list
+ * @h: list_t list
+ * Return: number of nodes in h
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
