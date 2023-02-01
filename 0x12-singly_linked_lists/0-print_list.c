185b299a0a17ab20df890f2f9a6287709d805063
Justine Tihno <justintihno@gmail.com>
2023-01-31 21:56:20 -0800

    task

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,13 +4,14 @@
 /**
  * print_list - prints all elements of a list
  * @h: pointer to the head of the list
- * Return: number of nodes
+ * Return: number of counts
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	while (h)
+
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
