590b80d19e1694feb7e9ce517dd0768ccf9d9efb
Justin <justintihno@gmail.com>
2023-02-13 11:48:24 +0300

    task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,14 +10,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	if (!b) return (0);
-
 	unsigned int result = 0;
 	int i = 0;
 
+	if (!b)
+		return (0);
+
 	while (b[i] != '\0')
 	{
-		if (b[i] != '0' && b[i] != '1') return (0);
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
 
 		result = result * 2 + (b[i] - '0');
 
