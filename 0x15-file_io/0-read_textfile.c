43b4fd55b41c03fb5b03fe16b7f60c3f623c0f31
Justin <justintihno@gmail.com>
2023-02-20 12:30:38 +0300

    task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,38 +9,44 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+    FILE *fp;
+    char *buffer;
+    ssize_t bytes_written;
+    size_t bytes_read;
+
     if (filename == NULL) {
-        return 0;
+        return (0);
     }
 
-    FILE *fp = fopen(filename, "r");
+    fp = fopen(filename, "r");
     if (fp == NULL) {
-        return 0;
+        return (0);
     }
 
-    char *buffer = (char *)malloc(sizeof(char) * (letters + 1));
+    buffer = (char *)malloc(sizeof(char) * (letters + 1));
     if (buffer == NULL) {
         fclose(fp);
-        return 0;
+        return (0);
     }
 
-    size_t bytes_read = fread(buffer, sizeof(char), letters, fp);
+    bytes_read = fread(buffer, sizeof(char), letters, fp);
     if (bytes_read == 0) {
         fclose(fp);
         free(buffer);
-        return 0;
+        return (0);
     }
 
     buffer[bytes_read] = '\0';
 
-    ssize_t bytes_written = printf("%s", buffer);
+    bytes_written = write(1, buffer, bytes_read);
     if (bytes_written < 0 || (size_t)bytes_written != bytes_read) {
         fclose(fp);
         free(buffer);
-        return 0;
+        return (0);
     }
 
     fclose(fp);
     free(buffer);
-    return bytes_read;
+    return (bytes_read);
 }
+
